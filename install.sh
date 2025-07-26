0685c50d86d88792ec5f6da7b557b2d65af3c804
s3wag <76864470+s3wag@users.noreply.github.com>
2025-07-26 14:31:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "[+] Installing Go tools..."
 
 go install github.com/tomnomnom/assetfinder@latest
 go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install github.com/cihanmehmet/shosubgo@latest
+go install github.com/incogbyte/shosubgo@latest
 go install github.com/gwen001/github-subdomains@latest
 go install github.com/gwen001/gitlab-subdomains@latest
 go install github.com/projectdiscovery/chaos-client/cmd/chaos@latest
@@ -22,9 +22,10 @@ go install github.com/hakluke/hakrawler@latest
 go install github.com/projectdiscovery/katana/cmd/katana@latest
 go install github.com/jaeles-project/gospider@latest
 go install github.com/tomnomnom/unfurl@latest
-go install github.com/s0md3v/smap@latest
+go install github.com/s0md3v/smap/cmd/smap@latest
 go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install github.com/0xKayala/jsleak@latest
+go install github.com/glebarez/cero@latest
 
 #sudo cp -r ~/go/bin/* /usr/local/bin/
 
