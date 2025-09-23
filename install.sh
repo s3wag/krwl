43e1025d0f6dc1a48107132583adcaa8496ee6c7
s3wag <76864470+s3wag@users.noreply.github.com>
2025-09-23 15:30:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "[+] Updating packages..."
 sudo apt update && sudo apt-get update
-sudo apt install -y golang amass jq
+sudo apt install -y golang amass jq whatweb nuclei
+pipx install uro
 
 echo "[+] Installing Go tools..."
 
@@ -26,6 +27,10 @@ go install github.com/s0md3v/smap/cmd/smap@latest
 go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 go install github.com/channyein1337/jsleak@latest
 go install github.com/glebarez/cero@latest
+go install github.com/tomnomnom/gf@latest && git clone https://github.com/1ndianl33t/Gf-Patterns.git ~/.gf
+go install github.com/tomnomnom/qsreplace@latest
+go install github.com/ferreiraklet/airixss@latest
+go install github.com/takshal/freq@latest
 
 sudo cp -r ~/go/bin/* /usr/local/bin/
 
